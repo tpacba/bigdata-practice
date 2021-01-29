d95f12965190ad7a6e2af803db82947571a4431e
william <william.pacba@enhanceit.us>
2021-01-28 23:43:55 -0500

    one.sql

---
+++ sql-practice/one.sql
@@ -0,0 +1,19 @@
+use test;
+
+create table tab1 if not exists(
+	s_no int,
+	col1 varchar(1),
+	col2 varchar(1),
+	col3 varchar(1),
+	col4 varchar(1),
+	col5 varchar(1)
+)
+
+select * from tab1;
+truncate table tab1;
+
+insert into tab1(s_no,col1,col2,col3,col4,col5) values (1,'a','b','c','d','e');
+insert into tab1(s_no,col1,col2,col3,col4,col5) values (2,null,'a','b','c','d');
+insert into tab1(s_no,col1,col2,col3,col4,col5) values (3,'e',null,null,null,'a');
+insert into tab1(s_no,col1,col2,col3,col4,col5) values (4,null,'a','e',null,'d');
+insert into tab1(s_no,col1,col2,col3,col4,col5) values (5,'e','d','c','b',null);
