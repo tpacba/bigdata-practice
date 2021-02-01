71a9d53480d4da76619d150fd8ce132a4f04ba72
Tristan Pacba <tpacba@live.com>
2021-02-01 15:55:29 -0500

    five.sql added

---
+++ sql-practice/five.sql
@@ -0,0 +1,22 @@
+use test;
+
+-- write a query to print numbers 1 to n --
+
+create table if not exists tab5(
+	rownum int
+)
+
+delimiter //
+create procedure num_iteration(n int)
+begin
+	declare i int default 0;
+    while i <= n do
+		insert into tab5(rownum) values (i);
+        set i = i + 1;
+    end while;
+end
+//
+
+call num_iteration(10);
+
+select * from tab5;
