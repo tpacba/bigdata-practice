3e88e714c5d03ee240e629ab2e8351105262ddb2
Tristan Pacba <tpacba@live.com>
2021-02-01 06:59:22 -0500

    finished one.sql

--- sql-practice/one.sql
+++ sql-practice/one.sql
@@ -1,6 +1,6 @@
 use test;
 
-create table tab1 if not exists(
+create table if not exists tab1(
 	s_no int,
 	col1 varchar(1),
 	col2 varchar(1),
@@ -17,3 +17,6 @@ insert into tab1(s_no,col1,col2,col3,col4,col5) values (2,null,'a','b','c','d');
 insert into tab1(s_no,col1,col2,col3,col4,col5) values (3,'e',null,null,null,'a');
 insert into tab1(s_no,col1,col2,col3,col4,col5) values (4,null,'a','e',null,'d');
 insert into tab1(s_no,col1,col2,col3,col4,col5) values (5,'e','d','c','b',null);
+
+-- show rows that contain 'a' without using the 'OR' statement --
+select * from tab1 where 'a' in (col1, col2,col3,col4,col5);
