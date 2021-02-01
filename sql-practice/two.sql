eb12ae8c3f85cfbe525390f6e5df0a567a4085d9
william <william.pacba@enhanceit.us>
2021-02-01 08:23:59 -0500

    answered two.sql

--- sql-practice/two.sql
+++ sql-practice/two.sql
@@ -12,5 +12,20 @@ select * from tab2;
 insert into tab2(ename, job, sal) values ('KING', 'PRESIDENT', 5100);
 insert into tab2(ename, job, sal) values ('BLAKE', 'MANAGER', 2850);
 insert into tab2(ename, job, sal) values ('CLARK', 'MANAGER', 2550);
-insert into tab2(ename, job, sal) values ('JONES', 'MANAGER', 2550);
-insert into tab2(ename, job, sal) values ('SCOTT', 'ANALYST', 2550);
+insert into tab2(ename, job, sal) values ('JONES', 'MANAGER', 2975);
+insert into tab2(ename, job, sal) values ('SCOTT', 'ANALYST', 3000);
+insert into tab2(ename, job, sal) values ('FORD', 'ANALYST', 3000);
+insert into tab2(ename, job, sal) values ('SMITH', 'CLERK', 800);
+insert into tab2(ename, job, sal) values ('ALLEN', 'SALESMAN', 1600);
+insert into tab2(ename, job, sal) values ('WARD', 'SALESMAN', 1250);
+insert into tab2(ename, job, sal) values ('MARTIN', 'SALESMAN', 1250);
+insert into tab2(ename, job, sal) values ('TURNER', 'SALESMAN', 1500);
+insert into tab2(ename, job, sal) values ('ADAMS', 'CLERK', 1100);
+insert into tab2(ename, job, sal) values ('JAMES', 'CLERK', 950);
+insert into tab2(ename, job, sal) values ('MILLER', 'CLERK', 1400);
+
+
+-- SHOW EMPLOYEES GETTING SALARY GREAETER THAN AVERAGE SALARY --
+select * from tab2 where sal > (select avg(sal) from tab2);
+
+
