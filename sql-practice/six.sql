fc417cb801103f91ef37192b82cf6d881228eab0
Tristan Pacba <tpacba@live.com>
2021-02-01 16:40:37 -0500

    six.sql finished

--- sql-practice/six.sql
+++ sql-practice/six.sql
@@ -6,5 +6,7 @@ select rownum,
 substring('welcome', rownum, 1) as output1,
 substring('welcome', 8 - rownum, 1) as output2,
 substring('welcome', 1, rownum) as output3,
-substring('welcome', rownum) as output4
+substring('welcome', rownum) as output4,
+concat(rpad('', rownum, ' '), substring('welcome', rownum)) as output5,
+concat(rpad('', 9 - rownum, ' '), substring('welcome', 1, rownum)) as output6
 from tab5 where rownum > 0 and rownum < 8;
