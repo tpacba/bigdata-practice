4d9bbb609ef6ce66a9724f4b17e8e75716959ef7
william <william.pacba@enhanceit.us>
2021-02-01 09:19:55 -0500

    three.sql

---
+++ sql-practice/three.sql
@@ -0,0 +1,27 @@
+use test;
+
+create table if not exists tab3a (
+	c1 char(1)
+);
+
+create table if not exists tab3b (
+	c1 char(1)
+);
+
+select * from tab3a;
+select * from tab3b;
+
+insert into tab3a(c1) values ('A');
+insert into tab3a(c1) values ('B');
+insert into tab3a(c1) values ('C');
+insert into tab3a(c1) values ('D');
+insert into tab3a(c1) values ('E');
+
+insert into tab3b(c1) values ('A');
+insert into tab3b(c1) values ('C');
+insert into tab3b(c1) values ('E');
+insert into tab3b(c1) values ('G');
+
+
+-- select data from tab3a that don't exits in tab3b without using 'NOT' keyword --
+select * from tab3a where 1 > (select count(*) from tab3b where tab3a.c1 = tab3b.c1);
