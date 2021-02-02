884179d6c264091ea5d3b32897a11d87f0004977
Tristan Pacba <tpacba@live.com>
2021-02-01 20:56:50 -0500

    added seven.sql

---
+++ sql-practice/seven.sql
@@ -0,0 +1,39 @@
+use test;
+
+create table if not exists tab6 (
+	mail_id varchar(255)
+);
+
+insert into tab6(mail_id) values ('siva.k.academy@gmail.com');
+insert into tab6(mail_id) values ('jeevan.anand.kumar@yahoo.com');
+insert into tab6(mail_id) values ('ravi.guru.raj@outlook.com');
+
+
+-- write a query to extract first, middle, last name and email domain from given email id --
+
+with fn as (
+	select 
+		mail_id as email,
+		substring(mail_id,1,regexp_instr(mail_id, '\\.') - 1) as first_name
+	from tab6
+),
+mn as (
+	select 
+		email, 
+		first_name,
+        substring(replace(email,concat(first_name,'.'),''), 1, regexp_instr(replace(email,concat(first_name,'.'),''), '\\.') - 1) as middle_name
+	from fn
+),
+ln as (
+	select email,
+		first_name,
+		middle_name,
+		substring(email, length(concat(first_name,'.',middle_name)) + 2,regexp_instr(email,'@') - (length(concat(first_name,'.',middle_name)) + 2)) as last_name
+	from mn
+) select 
+	email,
+    first_name,
+    middle_name,
+    last_name,
+    substring(email, regexp_instr(email,'@') + 1) as domain_name
+ from ln;
