0649f42c26bace7c80cbcb8ddc6b255bfa2ce2c5
Tristan Pacba <tpacba@live.com>
2021-02-18 13:16:54 -0500

    Add files via upload

---
+++ dags/create-external-tables.hql
@@ -0,0 +1,83 @@
+use marketingdb;
+
+-- MYSQL_TABLES --
+
+drop table if exists users;
+
+create external table users(user_id int, first_name string, last_name string, birthdate date, city string, state string, country string)
+row format delimited
+fields terminated by ','
+location '/user/hadoop/input/mysql/users';
+
+drop table if exists user_socials;
+
+create external table user_socials(id int, user_id int, social_site string, username_used string, email_used string)
+row format delimited
+fields terminated by ','
+location '/user/hadoop/input/mysql/user_socials';
+
+
+
+-- POSTGRESQL_TABLES --
+
+drop table if exists song_listened;
+
+create external table song_listened(id int, song_id int, date_listened date)
+row format delimited
+fields terminated by ','
+location '/user/hadoop/input/postgresql/song_listened';
+
+drop table if exists youtube_watched;
+
+create external table youtube_watched(id int, video_id int, date_watched date)
+row format delimited
+fields terminated by ','
+location '/user/hadoop/input/postgresql/youtube_watched';
+
+drop table if exists artist_events;
+
+create external table artist_events(event_id int, event_name string, event_date date, artist_id int, city string, state string, country string)
+row format delimited
+fields terminated by ','
+location '/user/hadoop/input/postgresql/artist_events';
+
+drop table if exists creator_events;
+
+create external table creator_events(event_id int, event_name string, event_date date, creator_id int, city string, state string, country string)
+row format delimited
+fields terminated by ','
+location '/user/hadoop/input/postgresql/creator_events';
+
+
+
+-- ORACLE TABLES --
+
+drop table if exists spotify_artists;
+
+create external table spotify_artists(artist_id int, name string)
+row format delimited
+fields terminated by ','
+location '/user/hadoop/input/oracle/spotify_artists';
+
+drop table if exists spotify_songs;
+
+create external table spotify_songs(song_id int, song_name string, artist_id int)
+row format delimited
+fields terminated by ','
+location '/user/hadoop/input/oracle/spotify_songs';
+
+drop table if exists youtube_creators;
+
+create external table youtube_creators(creator_id int, username string)
+row format delimited
+fields terminated by ','
+location '/user/hadoop/input/oracle/youtube_creators';
+
+drop table if exists youtube_videos;
+
+create external table youtube_videos(video_id int, video_name string, creator_id int)
+row format delimited
+fields terminated by ','
+location '/user/hadoop/input/oracle/youtube_videos';
+
+show tables;
---
+++ dags/create-jobs.sh
@@ -0,0 +1,102 @@
+MYSQL_TABLES="users
+user_socials"
+
+POSTGRES_TABLES="song_listened
+youtube_watched
+artist_events
+creator_events
+"
+
+ORACLE_TABLES="
+spotify_artists
+spotify_songs
+youtube_creators
+youtube_videos
+"
+
+
+for m in $MYSQL_TABLES
+do
+	echo "Deleting mysql-import-$m..."
+	sqoop job --delete mysql-import-$m	
+	
+	echo "Creating mysql-import-$m..."
+	sqoop job --create mysql-import-$m \
+	-- import \
+	--connect jdbc:mysql://localhost/marketing \
+	--username root \
+	--password-file /user/hadoop/pwd/mysql-pass.txt \
+	--table $m -m 1 \
+	--target-dir /user/hadoop/input/mysql/$m
+done
+
+
+for p in $POSTGRES_TABLES
+do
+	echo "Deleting postgres-import-$p..."
+	sqoop job --delete postgres-import-$p
+	
+	echo "Creating postgres-import-$p..."
+	sqoop job --create postgres-import-$p \
+	-- import \
+	--connect jdbc:postgresql://localhost:5432/postgres \
+	--username postgres \
+	--password-file /user/hadoop/pwd/postgres-pass.txt \
+	--table $p -m 1 \
+	--target-dir /user/hadoop/input/postgresql/$p
+done
+
+
+for o in $ORACLE_TABLES
+do
+	echo "Deleting oracle-import-$o"
+	sqoop job --delete oracle-import-$o
+done	
+
+
+echo "Creating oracle-import-spotify_artists"
+sqoop job --create oracle-import-spotify_artists \
+-- import \
+--connect jdbc:oracle:thin:@localhost:1521:xe \
+--username hr \
+--password-file /user/hadoop/pwd/oracle-pass.txt \
+--table spotify_artists \
+--columns artist_id,name \
+--target-dir /user/hadoop/input/oracle/spotify_artists -m 1
+	
+
+echo "Creating oracle-import-spotify_songs"
+sqoop job --create oracle-import-spotify_songs \
+-- import \
+--connect jdbc:oracle:thin:@localhost:1521:xe \
+--username hr \
+--password-file /user/hadoop/pwd/oracle-pass.txt \
+--table spotify_songs \
+--columns song_id,song_name,artist_id \
+--target-dir /user/hadoop/input/oracle/spotify_songs -m 1
+
+
+echo "Creating oracle-import-youtube_creator"
+sqoop job --create oracle-import-youtube_creators \
+-- import \
+--connect jdbc:oracle:thin:@localhost:1521:xe \
+--username hr \
+--password-file /user/hadoop/pwd/oracle-pass.txt \
+--table youtube_creators \
+--columns creator_id,username \
+--target-dir /user/hadoop/input/oracle/youtube_creators -m 1
+	
+
+echo "Creating oracle-import-youtube_videos"
+sqoop job --create oracle-import-youtube_videos \
+-- import \
+--connect jdbc:oracle:thin:@localhost:1521:xe \
+--username hr \
+--password-file /user/hadoop/pwd/oracle-pass.txt \
+--table youtube_videos \
+--columns video_id,video_name,creator_id \
+--target-dir /user/hadoop/input/oracle/youtube_videos -m 1
+
+
+sqoop job --list
+echo "Bye"
